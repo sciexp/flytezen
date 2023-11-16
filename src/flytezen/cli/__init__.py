5cabe515f3a1fd79b9e02f3548199f2f63947488
Cameron Smith <cameron.ray.smith@gmail.com>
2023-11-16 09:35:48 -0500

    fix(cli): disable click Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/flytezen/cli/__init__.py
+++ src/flytezen/cli/__init__.py
@@ -1,19 +1,5 @@
-import rich_click as click
-
-from flytezen import __version__
 from flytezen.cli.execute import main as execute_main
 
 
-@click.group(
-    context_settings={"help_option_names": ["-h", "--help"]},
-    invoke_without_command=True,
-)
-@click.version_option(version=__version__, prog_name="flytezen")
 def main():
-    click.echo("flytezen")
-
-
-@main.command("execute")
-@click.argument("args", nargs=-1, type=click.UNPROCESSED)
-def execute(args):
-    execute_main(args)
+    execute_main()
