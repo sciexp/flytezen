4fe5d06184307fa1fd7376068b43e759ad2133b3
Cameron Smith <cameron.ray.smith@gmail.com>
2023-11-16 09:33:58 -0500

    test(cli): enable click with hydra Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/flytezen/cli/__init__.py
+++ src/flytezen/cli/__init__.py
@@ -1,23 +1,19 @@
-# import rich_click as click
+import rich_click as click
 
-# from flytezen import __version__
+from flytezen import __version__
 from flytezen.cli.execute import main as execute_main
 
-# from flytezen.logging_utils import configure_logging
 
-# logger = configure_logging("flytezen")
-
-# @click.group(
-#     context_settings={"help_option_names": ["-h", "--help"]},
-#     invoke_without_command=True,
-# )
-# @click.version_option(version=__version__, prog_name="flytezen")
-# def main():
-#     click.echo("flytezen")
-
-
-# @main.command()
-# def execute():
-#     execute_main(logger)
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
+@click.version_option(version=__version__, prog_name="flytezen")
 def main():
-    execute_main()
+    click.echo("flytezen")
+
+
+@main.command("execute")
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
+def execute(args):
+    execute_main(args)
