66dd8688101370ab9b14290a5cf771d0f6198ff6
Cameron Smith <cameron.ray.smith@gmail.com>
2023-11-24 08:16:54 -0500

    test(lrwine): use LR outputs Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/flytezen/workflows/lrwine.py
+++ src/flytezen/workflows/lrwine.py
@@ -73,21 +73,21 @@ def process_data(data: pd.DataFrame) -> pd.DataFrame:
 @task
 def train_model(
     data: pd.DataFrame, logistic_regression: LogisticRegressionInterface
-) -> str:
-    # ) -> LogisticRegression:
+# ) -> str:
+    ) -> LogisticRegression:
     """
     Train a model on the wine dataset.
     """
     features = data.drop("target", axis="columns")
     target = data["target"]
     logger.info(f"{pformat(logistic_regression)}\n\n")
-    model = LogisticRegressionInstance = LogisticRegression(
+    model = LogisticRegression(
         **asdict(logistic_regression)
     )
     model_path = "logistic_regression_model.joblib"
     joblib.dump(model, model_path)
-    return model_path
-    # return model
+    # return model_path
+    return model
 
 
 @workflow
@@ -95,8 +95,8 @@ def training_workflow(
     logistic_regression: LogisticRegressionInterface = LogisticRegressionInterface(
         max_iter=2000
     ),
-) -> str:
-    # ) -> LogisticRegression:
+# ) -> str:
+    ) -> LogisticRegression:
     """
     Put all of the steps together into a single workflow.
     """
