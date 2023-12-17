7106d072b8441086ca54dc26526824f9181bbd4f
Cameron Smith <cameron.ray.smith@gmail.com>
2023-12-17 18:36:53 -0500

    fix(flake): add formatter Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -29,93 +29,98 @@
     ];
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
 
-      perSystem = {self', system, ... }: 
-        let
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [inputs.poetry2nix.overlays.default];
+      perSystem = {
+        self',
+        system,
+        ...
+      }: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.poetry2nix.overlays.default];
+        };
+
+        pyPkgsBuildRequirements = {
+          cloudpickle = ["flit-core"];
+          feather-format = ["setuptools"];
+          flytekit = ["setuptools"];
+          flyteidl = ["setuptools"];
+          hydra-core = ["setuptools"];
+          hydra-joblib-launcher = ["setuptools"];
+          hydra-zen = ["setuptools"];
+          marshmallow-jsonschema = ["setuptools"];
+          xdoctest = ["setuptools"];
+        };
+
+        poetry2nixOverrides = pkgs.poetry2nix.overrides.withDefaults (
+          self: super: let
+            buildInputsOverrides =
+              builtins.mapAttrs (
+                package: buildRequirements:
+                  (builtins.getAttr package super).overridePythonAttrs (old: {
+                    buildInputs =
+                      (old.buildInputs or [])
+                      ++ (builtins.map (pkg:
+                        if builtins.isString pkg
+                        then builtins.getAttr pkg super
+                        else pkg)
+                      buildRequirements);
+                  })
+              )
+              pyPkgsBuildRequirements;
+          in
+            buildInputsOverrides
+            // {
+              hydra-core = super.hydra-core.override {preferWheel = true;};
+              hydra-joblib-launcher = super.hydra-joblib-launcher.override {preferWheel = true;};
+              scipy = super.scipy.override {preferWheel = true;};
+              yarl = super.yarl.override {preferWheel = true;};
+            }
+        );
+
+        poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          python = pkgs.python310;
+          preferWheels = false;
+          editablePackageSources = {
+            flytezen = ./src;
           };
+          groups = [
+            "test"
+          ];
+          checkGroups = ["test"];
+          extras = [];
+          overrides = poetry2nixOverrides;
+        };
+      in {
+        formatter = pkgs.alejandra;
 
-          pyPkgsBuildRequirements = {
-            cloudpickle = ["flit-core"];
-            feather-format = ["setuptools"];
-            flytekit = ["setuptools"];
-            flyteidl = ["setuptools"];
-            hydra-core = ["setuptools"];
-            hydra-joblib-launcher = ["setuptools"];
-            hydra-zen = ["setuptools"];
-            marshmallow-jsonschema = ["setuptools"];
-            xdoctest = ["setuptools"];
-          };
+        devShells = {
+          default = pkgs.mkShell {
+            name = "flytezen";
+            buildInputs = with pkgs; [
+              poetryEnv
+              # python310Packages.poetry-core
+              poetry
 
-          poetry2nixOverrides = pkgs.poetry2nix.overrides.withDefaults (
-            self: super: let
-              buildInputsOverrides =
-                builtins.mapAttrs (
-                  package: buildRequirements:
-                    (builtins.getAttr package super).overridePythonAttrs (old: {
-                      buildInputs =
-                        (old.buildInputs or [])
-                        ++ (builtins.map (pkg:
-                          if builtins.isString pkg
-                          then builtins.getAttr pkg super
-                          else pkg)
-                        buildRequirements);
-                    })
-                )
-                pyPkgsBuildRequirements;
-            in
-              buildInputsOverrides
-              // {
-                hydra-core = super.hydra-core.override {preferWheel = true;};
-                hydra-joblib-launcher = super.hydra-joblib-launcher.override {preferWheel = true;};
-                scipy = super.scipy.override {preferWheel = true;};
-                yarl = super.yarl.override {preferWheel = true;};
-              }
-          );
-
-          poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-            python = pkgs.python310;
-            preferWheels = false;
-            editablePackageSources = {
-              flytezen = ./src;
-            };
-            groups = [
-              "test"
+              atuin
+              bat
+              gh
+              git
+              gnumake
+              lazygit
+              poethepoet
+              ripgrep
+              starship
+              tree
+              yq-go
+              zsh
             ];
-            checkGroups = ["test"];
-            extras = [];
-            overrides = poetry2nixOverrides;
-          };
-        in {
-          devShells = {
-            default = pkgs.mkShell {
-              name = "flytezen";
-              buildInputs = with pkgs; [
-                poetryEnv
-                # python310Packages.poetry-core
-                poetry
-
-                atuin
-                bat
-                gh
-                git
-                gnumake
-                lazygit
-                poethepoet
-                ripgrep
-                starship
-                tree
-                yq-go
-                zsh
-              ];
-            };
           };
         };
       };
+    };
 }
